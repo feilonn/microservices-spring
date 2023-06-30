0a4bfe42784ad39c5cc44b75e17c3dc4ba2cde55
Raul Correia <raulvinicius245@gmail.com>
2023-06-30 15:21:43 -0300

    feat: Implementado endpoints para primeiro microserviço.

---
+++ hr-worker/src/main/java/com/raulcorreia/hrworker/entities/Worker.java
@@ -0,0 +1,55 @@
+package com.raulcorreia.hrworker.entities;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "tb_worker")
+public class Worker {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private Double dailyIncome;
+
+    public Worker() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getDailyIncome() {
+        return dailyIncome;
+    }
+
+    public void setDailyIncome(Double dailyIncome) {
+        this.dailyIncome = dailyIncome;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Worker worker = (Worker) o;
+
+        return id.equals(worker.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id.hashCode();
+    }
+}
---
+++ hr-worker/src/main/java/com/raulcorreia/hrworker/resources/WorkerResource.java
@@ -0,0 +1,35 @@
+package com.raulcorreia.hrworker.resources;
+
+import com.raulcorreia.hrworker.entities.Worker;
+import com.raulcorreia.hrworker.repositories.WorkerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/workers")
+public class WorkerResource {
+
+    @Autowired
+    private WorkerRepository repository;
+
+    @GetMapping
+    public ResponseEntity<List<Worker>> findAll() {
+        List<Worker> workers = repository.findAll();
+
+        return ResponseEntity.ok(workers);
+    }
+
+   @GetMapping("/{id}")
+   public ResponseEntity<Worker> findById(@PathVariable Long id) {
+        Worker worker = repository.findById(id).get();
+
+        return ResponseEntity.ok(worker);
+   }
+
+}
